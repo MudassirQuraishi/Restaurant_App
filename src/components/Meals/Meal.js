f9a83dcfc30d9a84f0661664d6e20003dfb0ba3d
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2024-01-01 18:22:24 +0530

    Order form added

--- src/components/Meals/Meal.js
+++ src/components/Meals/Meal.js
@@ -3,12 +3,21 @@ const Meals = (props) => {
     const meal = props.mealData;
     return (
         <>
-            <div className={classes.meal}>
-                <p className={classes.name}>{meal.name}</p>
-                <p>
-                    <i>{meal.description}</i>
-                </p>
-                <p className={classes.price}> ${meal.price} </p>
+            <div className={classes["meal-container"]}>
+                <div className={classes.meal}>
+                    <p className={classes.name}>{meal.name}</p>
+                    <p>
+                        <i>{meal.description}</i>
+                    </p>
+                    <p className={classes.price}> ${meal.price} </p>
+                </div>
+                <div className={classes["input-field"]}>
+                    <div>
+                        <label htmlFor='amount'>Amount: </label>
+                        <input type='number' placeholder='Enter Quantity' />
+                    </div>
+                    <button>Add</button>
+                </div>
             </div>
             <hr />
         </>
